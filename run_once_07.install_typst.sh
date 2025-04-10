b1f395d4a1a9c7a949db51f626eb47a9460477c1
Nicolas Deutschmann <nicolas@cradle.bio>
2025-04-10 09:18:13 +0200

    update typst

--- run_once_07.install_typst.sh
+++ run_once_07.install_typst.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # 
 export TYPST_INSTALL="${HOME}/.local"
-curl -fsSL https://typst.community/typst-install/install.sh | sh -s -- 0.12.0
-curl --proto '=https' --tlsv1.2 -LsSf https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.12.18/tinymist-installer.sh | sh
+curl -fsSL https://typst.community/typst-install/install.sh | sh -s -- 0.13.1
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.13.10/tinymist-installer.sh | sh
